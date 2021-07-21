b41bed423e6f1462ff878f65902ec39933e9ea87
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-21 21:36:17 +0900

    [chore] Project setting

---
+++ Captured-Prac/src/main/java/com/example/CapturedPrac/CapturedPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.CapturedPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapturedPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapturedPracApplication.class, args);
+	}
+
+}
---
+++ Captured-Prac/src/test/java/com/example/CapturedPrac/CapturedPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CapturedPrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapturedPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
